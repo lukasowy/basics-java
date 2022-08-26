1263bddf64de2fe1bf68acdf3bdfe1a7e1bb02e7
Lukasz Sosnowy <lukasowy@gmail.com>
2022-08-26 20:34:25 +0200

    Create Main.java

---
+++ src/main/java/lambda/Main.java
@@ -0,0 +1,14 @@
+package lambda;
+
+import java.util.List;
+
+public class Main {
+
+
+
+  public static void main(String[] args) {
+    List<Person> people = Person.generateDefaultList();
+
+
+  }
+}
